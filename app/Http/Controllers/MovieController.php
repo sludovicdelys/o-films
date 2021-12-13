0bc42a5fdc1cffd20b362da18ac788a5e4957f1a
sludovidelys <ludovicdelyssabrina@gmail.com>
2021-12-13 10:55:20 +0100

    models & controllers set up with php serve

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -6,8 +6,5 @@ use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    public function index()
-    {
-        return view('movie.index');
-    }
-} 
+    //
+}
--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -6,8 +6,5 @@ use Illuminate\Http\Request;
 
 class SerieController extends Controller
 {
-    public function index()
-    {
-        return view('serie.index');
-    }
+    //
 }
