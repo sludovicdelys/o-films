3740cd6f6e05eaa36d4698c4d5f257f47049a527
sludovidelys <ludovicdelyssabrina@gmail.com>
2021-12-13 11:43:16 +0100

    database-seeder file config ok

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\MovieSeeder;
+use Database\Seeders\SerieSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            MovieSeeder::class,
+            SerieSeeder::class
+        ]);
     }
 }
