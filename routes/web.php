0857f4f664e4e63de57c7b0eb1d6efa6d756f804
sludovidelys <ludovicdelyssabrina@gmail.com>
2022-01-08 17:59:58 +0100

    authentification correction/revisions

--- routes/web.php
+++ routes/web.php
@@ -38,22 +38,33 @@ Route::get('/login', [
 
 # https://laravel.com/docs/8.x/controllers#actions-handled-by-resource-controller
 
-// On définit les routes accessibles à tout le monde 
 
-// Génère toutes les routes qui sont liées à la ressource 'movies', par contre ne me génère que 'index' et 'show'
+// Definir les routes reservées aux utilisateurs connectés
+// Ici le middleware 'auth' verifie que l'utilisateur est identifié
+Route::group(['middleware' => 'auth'], function () {
+
+    // Les utilisateurs connectés ont accès à tout sauf à index et à show
+    Route::resource('movies', MovieController::class, ['except' => ['index', 'show']]);
+    Route::resource('series', SerieController::class, ['except' => ['index', 'show']]);
+
+    // Je peux me logout que quand je suis connecté 
+    Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
+});
+
+// Définir les routes réservées aux utilisateurs non-connectés 
+Route::group(['middleware' => 'guest'], function () {
+
+    Route::get('login', function () {
+        return view('auth.login');
+    })->name('auth.login');
+    
+    Route::post('login', [AuthController::class, 'login'])->name('auth.login.action');
+});
+
+// On définit les routes accessibles à tout le monde 
+// Génère toutes les routes qui sont liées à la ressource 'movies', par contre ne me génère que les routes'index' et 'show'.
 Route::resource('movies', MovieController::class, ['only' => ['index', 'show']]);
 Route::resource('series', SerieController::class, ['only' => ['index', 'show']]);
 
-// Definir les routes reservées aux utilisateurs connectés
-Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
 
-Route::get('login', function () {
-    return view('auth.login');
-})->name('auth.login');
-
-Route::post('login', [AuthController::class, 'login'])->name('auth.login.action');
-
-Route::get('form', function () {
-    return view('serie.form');
-})->name('serie.form');
 
