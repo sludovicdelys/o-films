ca6bfd843c10c63b8b164acaa1dfae50786ebc78
sludovidelys <ludovicdelyssabrina@gmail.com>
2022-01-08 18:04:16 +0100

    connexion sans les groupes de route

--- routes/web.php
+++ routes/web.php
@@ -41,30 +41,25 @@ Route::get('/login', [
 
 // Definir les routes reservées aux utilisateurs connectés
 // Ici le middleware 'auth' verifie que l'utilisateur est identifié
-Route::group(['middleware' => 'auth'], function () {
 
     // Les utilisateurs connectés ont accès à tout sauf à index et à show
-    Route::resource('movies', MovieController::class, ['except' => ['index', 'show']]);
-    Route::resource('series', SerieController::class, ['except' => ['index', 'show']]);
+    Route::resource('movies', MovieController::class);
+    Route::resource('series', SerieController::class);
 
     // Je peux me logout que quand je suis connecté 
     Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
-});
 
 // Définir les routes réservées aux utilisateurs non-connectés 
-Route::group(['middleware' => 'guest'], function () {
 
     Route::get('login', function () {
         return view('auth.login');
     })->name('auth.login');
     
     Route::post('login', [AuthController::class, 'login'])->name('auth.login.action');
-});
 
 // On définit les routes accessibles à tout le monde 
 // Génère toutes les routes qui sont liées à la ressource 'movies', par contre ne me génère que les routes'index' et 'show'.
-Route::resource('movies', MovieController::class, ['only' => ['index', 'show']]);
-Route::resource('series', SerieController::class, ['only' => ['index', 'show']]);
+
 
 
 
