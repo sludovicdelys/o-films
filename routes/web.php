9e1aacf649dc9b186496cbfff4f01419d96971d0
sludovidelys <ludovicdelyssabrina@gmail.com>
2022-01-08 19:09:40 +0100

    searchAPI route & previous middlewares

--- routes/web.php
+++ routes/web.php
@@ -16,50 +16,39 @@ use Illuminate\Support\Facades\Http;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-/*Route::get('/', function () {
-    return view('welcome');
-});
-
-// redirect to movies page
-Route::redirect('/', '/movies');
-
-// display movies page
-Route::get('/movies', [
-    'App\Http\Controllers\MovieController',
-    'create'
-])->name('movies.create');
-
-// display login page
-Route::get('/login', [
-    'App\Http\Controllers\SerieController',
-    'create'
-])->name('series.create'); */
+Route::redirect("/", "/movies");
 
 # https://laravel.com/docs/8.x/controllers#actions-handled-by-resource-controller
 
 
 // Definir les routes reservées aux utilisateurs connectés
 // Ici le middleware 'auth' verifie que l'utilisateur est identifié
+Route::middleware('auth')->group(function () {
+    // Je peux me logout que quand je suis connecté 
+    Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
 
     // Les utilisateurs connectés ont accès à tout sauf à index et à show
     Route::resource('movies', MovieController::class);
     Route::resource('series', SerieController::class);
 
-    // Je peux me logout que quand je suis connecté 
-    Route::get('logout', [AuthController::class, 'logout'])->name('auth.logout');
+    // Cette route renvoie vers la méthode searchApi qui se trouve dans mon ShowController
+    Route::get('/shows/searchApi', [ShowController::class, 'searchApi'])->name('shows.searchApi');
+});
 
 // Définir les routes réservées aux utilisateurs non-connectés 
+Route::middleware('guest')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])->name('auth.login.action');
 
     Route::get('login', function () {
         return view('auth.login');
     })->name('auth.login');
-    
-    Route::post('login', [AuthController::class, 'login'])->name('auth.login.action');
+});
 
 // On définit les routes accessibles à tout le monde 
 // Génère toutes les routes qui sont liées à la ressource 'movies', par contre ne me génère que les routes'index' et 'show'.
-
+// Attention a bien les mettre en bas du fichier de route pour eviter les problemes de priorités de routes
+Route::resource('movies', MovieController::class)->only(['index', 'show']);
+Route::resource('shows', ShowController::class)->only(['index', 'show']);
 
 
 
