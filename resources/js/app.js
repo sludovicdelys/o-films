709e5b8049ef41cce4765276a4cb42202eebdebd
Sabrina Ludovic de Lys <ludovicdelyssabrina@gmail.com>
2023-09-05 12:14:01 +0200

    build : add bootstrap config

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,2 @@
-/* import { Collapse } from 'bootstrap' */
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-/* document.addEventListener('DOMContentLoaded', function () {
-    // Initialize the Collapse component
-    new Collapse(document.getElementById('navbarNav'));
-}); */
--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -27,33 +27,9 @@
                         </a>
                     @endguest
                 </div>
+                   
 
-                <!-- <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled" href="#">Disabled</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="https://example.com" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                            <div class="dropdown-menu" aria-labelledby="dropdown03">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                        </li>
-                    </ul>
-                    <form class="form-inline my-2 my-md-0">
-                    <input class="form-control" type="text" placeholder="Search">
-                    </form>
-                </div> -->
-
-                <!-- <ul class="menu_section">
+                <ul class="menu_section">
                     @auth
                     <button class="user-menu__button navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExample10" aria-controls="navbarsExample10" aria-expanded="false" aria-label="Toggle navigation">
                         <span>{{ auth()->user()->name }}</span>
@@ -66,7 +42,7 @@
                         </ul>
                     </div> 
                     @endauth
-                </ul> -->
+                </ul>
             </div>
         </header>
         @yield('content')
