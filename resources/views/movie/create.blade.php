2ca1aa09108baf6ac45ebc0fe7b8a471d51ae6e4
Sabrina Ludovic de Lys <ludovicdelyssabrina@gmail.com>
2023-09-14 16:40:24 +0200

    style : custom validation styling for form submissions

--- resources/views/movie/create.blade.php
+++ resources/views/movie/create.blade.php
@@ -23,20 +23,22 @@
             -->
         </div>
         @endif
-        <form action="{{ route('movies.store') }}" method="post" class="row">
+        <form action="{{ route('movies.store') }}" method="post" class="row needs-validation" novalidate>
             @csrf
             <input type="hidden" name="id" value="{{ $movie->id }}">
             <div class="col-12 mb-3">
                 <label for="title" class="form-label">Titre</label>
                 <input value="{{ old('title', $movie->title) }}" type="text" class="form-control" name="title" id="title" required>
+                <div class="invalid-feedback">Veuillez fournir un titre.</div>
             </div>
             <div class="col-12 mb-3">
                 <label for="year" class="form-label">Année de sortie</label>
                 <input value="{{ old('year'), $movie->year }}" type="year" class="form-control" name="year" id="year" required>
+                <div class="invalid-feedback">Veuillez fournir l'année de sortie.</div>
             </div>
             <div class="col-12 mb-3">
-                <label for="note" class="form-label">Note</label>
-                <select  class="form-select" name="stars">
+                <label for="stars" class="form-label">Note</label>
+                <select  class="form-select" name="stars" required>
                     <option value="0" {{ old('stars', $movie->stars) == 0 ? 'selected' : '' }}>0</option>
                     <option value="1" {{ old('stars', $movie->stars) == 1 ? 'selected' : '' }}>1</option>
                     <option value="2" {{ old('stars', $movie->stars) == 2 ? 'selected' : '' }}>2</option>
@@ -47,12 +49,13 @@
             </div>
             <div class="col-12 mb-3">
                 <label for="review" class="form-label">Resumé</label>
-                <textarea value="{{ old('review'), $movie->review }}" class="form-control" id="review" rows="3"></textarea>
+                <textarea value="{{ old('review'), $movie->review }}" class="form-control" id="review" rows="3" required></textarea>
+                <div class="invalid-feedback">Veuillez fournir un résumé.</div>
             </div>
             <div class="col-12 mb-3">
                 <label class="form-label">Pays</label>
-                <select class="form-select" name="country_id">
-                    <option>Mali</option>
+                <select class="form-select" name="country_id" required>
+                    <option selected disabled value="">Choisir...</option>
 
                     @foreach ($countries as $country)
                         <option value="{{ $country->id }}" @if (old('country_id', $movie->country_id) == $country->id )
@@ -60,11 +63,12 @@
                         @endif>{{ $country->name }}</option>
                     @endforeach
                 </select>
+                <div class="invalid-feedback">Veuillez fournir le pays de production.</div>
             </div>
             <div class="col-12 mb-3">
                 <label class="form-label">Genre</label>
-                <select class="form-select" name="genre_id">
-                    <option>Documentaire</option>
+                <select class="form-select" name="genre_id" required>
+                    <option selected disabled value="">Choisir...</option>
 
                     @foreach ($genres as $genre)
                         <option value="{{ $genre->id }}" @if (old('genre_id', $movie->genre_id) == $genre->id )
@@ -72,6 +76,7 @@
                         @endif>{{ $genre->name }}</option>
                     @endforeach
                 </select>
+                <div class="invalid-feedback">Veuillez fournir le genre.</div>
             </div>
             <div class="col-12 mb-3">
                 <button type="submit" class="btn btn-success">Soumettre</button>
--- resources/views/serie/create.blade.php
+++ resources/views/serie/create.blade.php
@@ -23,24 +23,28 @@
             -->
         </div>
         @endif
-        <form action="{{ route('series.store') }}" method="post" class="row">
+        <form action="{{ route('series.store') }}" method="post" class="row needs-validation" novalidate>
             @csrf
             <input type="hidden" name="id" value="{{ $serie->id }}">
             <div class="col-12 mb-3">
                 <label for="title" class="form-label">Titre</label>
                 <input value="{{ old('title', $serie->title) }}" name="title" class="form-control" name="title" id="title" required>
+                <div class="invalid-feedback">Veuillez fournir un titre.</div>
             </div>
             <div class="col-12 mb-3">
                 <label for="year" class="form-label">Année de sortie</label>
                 <input value="{{ old('year'), $serie->year }}" type="year" class="form-control" name="year" id="year" required>
+                <div class="invalid-feedback">Veuillez fournir l'année de sortie.</div>
             </div>
             <div class="col-md-6 mb-3">
                 <label for="seasons" class="form-label">Nombre de saisons</label>
-                <input value="{{ old('seasons', $serie->seasons )}}" name="seasons" min="0" max="5" class="form-control" type="number">
+                <input value="{{ old('seasons', $serie->seasons )}}" name="seasons" min="0" max="5" class="form-control" type="number" required>
+                <div class="invalid-feedback">Veuillez fournir le nombre de saisons.</div>
             </div>
             <div class="col-md-6 mb-3">
                 <label class="form-label">Nombre d'épisodes par saison</label>
-                <input value="{{ old('episodesPerSeason', $serie->episodesPerSeason )}}" name="episodesPerSeason" min="0" max="10" class="form-control" type="number">
+                <input value="{{ old('episodesPerSeason', $serie->episodesPerSeason )}}" name="episodesPerSeason" min="0" max="10" class="form-control" type="number" required>
+                <div class="invalid-feedback">Veuillez fournir le nombre d'épisodes par saison.</div>
             </div>
             <div class="col-12 mb-3">
                 <label for="note" class="form-label">Note</label>
@@ -55,12 +59,13 @@
             </div>
             <div class="col-12 mb-3">
                 <label class="form-label">Resumé</label>
-                <textarea value="{{ old('review'), $serie->review }}" class="form-control" id="review" rows="3"></textarea>
+                <textarea value="{{ old('review'), $serie->review }}" class="form-control" id="review" rows="3" required></textarea>
+                <div class="invalid-feedback">Veuillez fournir un résumé.</div>
             </div>
             <div class="col-12 mb-3">
                 <label class="form-label">Pays</label>
-                <select class="form-select" name="country_id">
-                    <option>Turquie</option>
+                <select class="form-select" name="country_id" required>
+                    <option selected disabled value="">Choisir...</option>
 
                     @foreach ($countries as $country)
                         <option value="{{ $country->id }}" @if (old('country_id', $serie->country_id) == $country->id )
@@ -68,11 +73,12 @@
                         @endif>{{ $country->name }}</option>
                     @endforeach
                 </select>
+                <div class="invalid-feedback">Veuillez fournir le pays de production.</div>
             </div>
             <div class="col-12 mb-3">
                 <label class="form-label">Genre</label>
-                <select class="form-select" name="genre_id">
-                    <option>Horreur</option>
+                <select class="form-select" name="genre_id" required>
+                    <option selected disabled value="">Choisir...</option>
 
                     @foreach ($genres as $genre)
                         <option value="{{ $genre->id }}" @if (old('genre_id', $serie->genre_id) == $genre->id )
@@ -80,6 +86,7 @@
                         @endif>{{ $genre->name }}</option>
                     @endforeach
                 </select>
+                <div class="invalid-feedback">Veuillez fournir le genre.</div>
             </div>
             <div class="col-12 mb-3">
                 <button type="submit" class="btn btn-success">Soumettre</button>
